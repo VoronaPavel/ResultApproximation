fbb7f68398966e4f4e6b1bfff6d4bcd85125c347
P.Vorona <p.vorona@itransition.com>
2015-10-13 16:45:55 +0300

    empty reducer

--- src/Main.scala
+++ src/Main.scala
@@ -2,6 +2,6 @@ object Main {
 
   def main(args: Array[String]) {
     val digits = (Node(3, "3"), Node(7, "7"))
-    println(digits)
+    println(Calculator)
   }
 }
--- src/Problem1.java
+++ src/Problem1.java
@@ -26,7 +26,8 @@ public class Problem1 {
     enum Reducer {
 
         Add((left, right) -> left + right, " + "),
-        Subtract((left, right) -> left - right, " - ");
+        Subtract((left, right) -> left - right, " - "),
+        Empty((left, right) -> left * 10 + right, "");
 
         private final BiFunction<Integer, Integer, Integer> reducer;
         private final String operator;
@@ -37,8 +38,7 @@ public class Problem1 {
         }
 
         Node reduce(Node left, Node right) {
-            return new Node(reducer.apply(left.value, right.value),
-                    left.representation + operator + right.representation);
+            return new Node(reducer.apply(left.value, right.value), left.representation + operator + right.representation);
         }
     }
 
@@ -61,5 +61,37 @@ public class Problem1 {
             return calculate(solutions);
         }
 
+        public static List<List<Node>> f(List<List<Node>> nodes) {
+            if (nodes.get(0).size() == 1) return nodes;
+
+            List<List<Node>> solutions = new ArrayList<>();
+            for (List<Node> n: nodes) {
+                for (int i = 0; i < n.size(); i++) {
+                    solutions.add(reduceLeft(n));
+                    reduceRight(n).forEach(c -> solutions.add(c));
+                }
+            }
+            return solutions;
+        }
+
+        private static List<List<Node>> reduceRight(List<Node> n) {
+            List<Node> subList = n.subList(1, n.size());
+            List<Node> wholeList = new ArrayList<>();
+            wholeList.add(n.get(0));
+            ArrayList<List<Node>> es = new ArrayList<>();
+            es.add(subList);
+            wholeList.addAll(f(es));
+            return wholeList;
+        }
+
+        private static List<Node> reduceLeft(List<Node> n) {
+            Node l = n.get(0);
+            Node r = n.get(1);
+            Node reduced = Reducer.Empty.reduce(l, r);
+            n.remove(1);
+            n.remove(0);
+            n.add(0, reduced);
+            return n;
+        }
     }
 }
